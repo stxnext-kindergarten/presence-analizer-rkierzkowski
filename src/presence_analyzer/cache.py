6017d7f85d93fc878a8dcc84a9b2a843a608b955
Roman Kierzkowski <roman.kierzkowski@stxnext.pl>
2014-04-17 14:29:39 +0200

    Cache

---
+++ src/presence_analyzer/cache.py
@@ -0,0 +1,27 @@
+import threading
+
+import time
+
+def timestamp():
+    return time.time()
+
+def cache(period):
+    def decorate(f):
+        store = {} # work a round for missing nonlocal declaration in 2.7
+        lock = threading.RLock()
+
+        def in_cache():
+            if 'result' in store:
+                print store['ts'], timestamp(), store['ts'] + period,
+            return 'result' in store and timestamp() < store['ts'] + period
+
+        def execute(*args, **kwargs):
+            with lock:
+                if not in_cache():
+                    store['ts'] = timestamp()
+                    result = f(*args, **kwargs)
+                    store['result'] = result
+                    return result
+                return store['result']
+        return execute
+    return decorate
